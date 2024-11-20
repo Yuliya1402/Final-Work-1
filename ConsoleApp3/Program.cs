4ad6cfa87551d4a1650e4e0496bb372a5731c30f
Yuliya Turmacheva <Yuliyat1402@gmail.com>
2024-11-20 09:34:12 +0300

    Add new code for additional functionality

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] initialArray = { "Hello", "2", "world", ":-)" };
+        string[] result = GetShortString(initialArray);
+        foreach (string i in result)
+        {
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+    static string[] GetShortString(string[] array)
+
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] result = new string[count];
+        int index = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                result[index] = array[i];
+                index++;
+            }
+        }
+        return result;
+    }
+}
